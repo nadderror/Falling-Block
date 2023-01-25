d98cc76832a56c955fcf3bbbcf99073ff26bbfa8
noon-ghe <naderghaliany@gmail.com>
2023-01-25 23:17:37 +0330

    player has some shadow on screen sides now

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 20;
+
+    private float playerHalfWidth;
+
     //screenHalfWidthInWorldUnits
     private float sHWIWU;
+    [SerializeField] private GameObject[] playerShadows = new GameObject[2];
+
     void Start()
     {
-        sHWIWU = Camera.main.orthographicSize * Camera.main.aspect;
+        playerHalfWidth = transform.localScale.x / 2;
+        sHWIWU = (Camera.main.orthographicSize * Camera.main.aspect); //;
+        float shadowXPos = transform.position.x + Camera.main.orthographicSize;
+        playerShadows[0].transform.position = new Vector2(-sHWIWU*2, transform.position.y);
+        playerShadows[1].transform.position = new Vector2(sHWIWU*2, transform.position.y);
     }
 
     void Update()
