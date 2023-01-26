e0ba9ad76d185485d7894fff4447bbb2ce3e5db6
noon-ghe <naderghaliany@gmail.com>
2023-01-26 21:30:24 +0330

    Enemy falling down option added

---
+++ Assets/_Scripts/EnemyFalling.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class EnemyFalling : MonoBehaviour
+{
+    private float fallingSpeed = 10;
+    void Update()
+    {
+        transform.Translate(Vector2.down*fallingSpeed * Time.deltaTime);
+    }
+}
